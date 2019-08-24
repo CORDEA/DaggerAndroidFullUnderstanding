3255b7671fe953be1c6e958d201b7f8879ca0f88
Yoshihiro Tanaka <contact@cordea.jp>
2019-08-24 21:25:59 +0900

    update layout

--- app/src/main/java/jp/cordea/daggerandroidfullunderstanding/ui/menu/MenuBottomSheetDialogFragment.kt
+++ app/src/main/java/jp/cordea/daggerandroidfullunderstanding/ui/menu/MenuBottomSheetDialogFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import jp.cordea.daggerandroidfullunderstanding.R
 
 class MenuBottomSheetDialogFragment : BottomSheetDialogFragment() {
     companion object {
+        private const val TAG = "MenuBottomSheetDialogFragment"
+
         fun newInstance() = MenuBottomSheetDialogFragment()
     }
 
@@ -18,5 +21,9 @@ class MenuBottomSheetDialogFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.menu_bottom_sheet_dialog_fragment, container, false)
+    }
+
+    fun show(manager: FragmentManager) {
+        show(manager, tag)
     }
 }
