087b71238664a3f0bd324c72b74374dee14ac70f
Yoshihiro Tanaka <contact@cordea.jp>
2019-08-24 21:44:54 +0900

    handle click event

--- app/src/main/java/jp/cordea/daggerandroidfullunderstanding/ui/menu/MenuBottomSheetDialogFragment.kt
+++ app/src/main/java/jp/cordea/daggerandroidfullunderstanding/ui/menu/MenuBottomSheetDialogFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import jp.cordea.daggerandroidfullunderstanding.R
+import kotlinx.android.synthetic.main.menu_bottom_sheet_dialog_fragment.*
 
 class MenuBottomSheetDialogFragment : BottomSheetDialogFragment() {
     companion object {
@@ -19,11 +21,21 @@ class MenuBottomSheetDialogFragment : BottomSheetDialogFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.menu_bottom_sheet_dialog_fragment, container, false)
+    ): View = inflater.inflate(R.layout.menu_bottom_sheet_dialog_fragment, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        create.setOnClickListener {
+            findNavController().navigate(R.id.createTagFragment)
+            dismiss()
+        }
+        edit.setOnClickListener {
+            findNavController().navigate(R.id.editTagFragment)
+            dismiss()
+        }
     }
 
     fun show(manager: FragmentManager) {
-        show(manager, tag)
+        show(manager, TAG)
     }
 }
